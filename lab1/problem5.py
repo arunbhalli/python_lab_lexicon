c112282b0e83d06ffd7f1dd827c5de8c7aaf58f6
arunbhalli <tomarbhalli@gmail.com>
2022-10-18 13:20:23 +0200

    use of format in quetion 5

---
+++ lab1/problem5.py
@@ -0,0 +1,10 @@
+# Problem 5:
+# You are given two variables:
+#     age = 4
+#     name = "Sammy"
+#     Use print formatting to print the following
+# string: "Hello my dog's name is Bobby and he is 4 years old"Good luck!
+
+age = 4
+name = "Sammy"
+print('Hello my dogs {name} is Bobby and he is {age} years old Good luck!'.format(name = "Sammy",age = 4))
