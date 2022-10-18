5fd7f11e5e525d5b86f03063c9546fa83e2dedec
arunbhalli <tomarbhalli@gmail.com>
2022-10-18 12:45:57 +0200

    adds solutions of quetion1

---
+++ problem1.py
@@ -0,0 +1,11 @@
+# Problem 1: Given the string:
+#     s = 'Lexicon'Use indexing to print the out the followinf:
+#     'L''n''Lexi''con''on'Bonus: Use indexing to reverse the string
+
+s ='Lexicon'
+print(s[0])
+print(s[-1])
+print(s[:4])
+print(s[4:])
+print(s[5:])
+print(s[::-1])
